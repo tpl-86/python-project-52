4874f7653d02f3097e62bcff83de073377af78cb
tpl-86 <tron-050686@mail.ru>
2025-10-17 15:06:47 +0300

    Changed build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
 
 echo "--> Installing dependencies with uv..."
-uv pip sync --no-cache
+uv pip sync pyproject.toml --no-cache
 
 echo "--> Collecting static files..."
 poetry run python manage.py collectstatic --no-input --clear
