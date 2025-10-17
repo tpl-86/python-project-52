d4955584d7cc956359d476360db45e9b612c0b03
tpl-86 <tron-050686@mail.ru>
2025-10-17 14:09:59 +0300

    Configured postgresql locally for Django

--- config/settings.py
+++ config/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
+        default=os.getenv('DATABASE_URL'),
         conn_max_age=600
     )
 }
