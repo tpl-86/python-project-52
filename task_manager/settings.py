83b1612a29f9a5289142fa6f450f5c77093000cc
tpl-86 <tron-050686@mail.ru>
2026-01-17 10:35:05 +0300

    Update settings.py and pyproject.toml.

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -84,11 +84,11 @@ TEMPLATES = [
             BASE_DIR / "task_manager" / "labels" / "templates",
         ],
         "APP_DIRS": True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
