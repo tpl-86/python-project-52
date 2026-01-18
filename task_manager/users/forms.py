5fe4229ae743a156eded74c7d502959b25045328
tpl-86 <tron-050686@mail.ru>
2026-01-18 10:23:34 +0300

    Update forms.py

--- task_manager/users/forms.py
+++ task_manager/users/forms.py
@@ -1,26 +1,77 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
-from django.core.exceptions import ValidationError
 
 
-class UserRegisterForm(UserCreationForm):
+class UserRegisterForm(forms.ModelForm):
+    # Стандартные поля формы
+    password1 = forms.CharField(
+        label="Password",
+        widget=forms.PasswordInput(attrs={"id": "id_password1", "class": "form-control"}),
+    )
+    password2 = forms.CharField(
+        label="Password confirmation",
+        widget=forms.PasswordInput(attrs={"id": "id_password2", "class": "form-control"}),
+    )
+
+    # Доп. алиасы для совместимости с тестами: password/password_confirmation
+    # (они будут маппиться в password1/password2 в __init__)
+    password = forms.CharField(
+        label="Password (alias)",
+        widget=forms.PasswordInput,
+        required=False,
+    )
+    password_confirmation = forms.CharField(
+        label="Password confirmation (alias)",
+        widget=forms.PasswordInput,
+        required=False,
+    )
+
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name')
-    
-    def _post_clean(self):
-        forms.ModelForm._post_clean(self)
-        pwd = self.cleaned_data.get('password2')
-        if pwd:
-            digits = sum(ch.isdigit() for ch in pwd)
-            if digits < 3:
-                msg = "Пароль должен содержать не менее 3 цифр."
-                # добавим ошибку к обоим полям пароля
-                self.add_error('password1', ValidationError(msg))
-                self.add_error('password2', ValidationError(msg))
+        fields = ("username", "first_name", "last_name")
+        widgets = {
+            "username": forms.TextInput(attrs={"id": "id_username", "class": "form-control"}),
+            "first_name": forms.TextInput(attrs={"id": "id_first_name", "class": "form-control"}),
+            "last_name": forms.TextInput(attrs={"id": "id_last_name", "class": "form-control"}),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Маппим альтернативные имена на стандартные, если они пришли
+        if self.data:
+            data = self.data.copy()
+            if not data.get("password1") and data.get("password"):
+                data["password1"] = data.get("password")
+            if not data.get("password2") and data.get("password_confirmation"):
+                data["password2"] = data.get("password_confirmation")
+            self.data = data
+
+    def clean(self):
+        cleaned = super().clean()
+        p1 = cleaned.get("password1") or ""
+        p2 = cleaned.get("password2") or ""
+
+        # Единственная проверка: длина >= 3
+        if len(p1) < 3:
+            self.add_error("password1", "Password must contain at least 3 characters.")
+        if p1 != p2:
+            self.add_error("password2", "Passwords do not match.")
+        return cleaned
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
 
 class UserUpdateForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name')
+        fields = ("username", "first_name", "last_name")
+        widgets = {
+            "username": forms.TextInput(attrs={"id": "id_username", "class": "form-control"}),
+            "first_name": forms.TextInput(attrs={"id": "id_first_name", "class": "form-control"}),
+            "last_name": forms.TextInput(attrs={"id": "id_last_name", "class": "form-control"}),
+        }
