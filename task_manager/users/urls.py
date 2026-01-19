6821e729f176a9cc014ba1d4b8b778edf25db112
tpl-86 <tron-050686@mail.ru>
2026-01-19 08:21:10 +0300

    Update users/urls.py

--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     path('', views.UserListView.as_view(), name='list'),
     path('create/', views.UserCreateView.as_view(), name='create'),
     path('<int:pk>/update/', views.UserUpdateView.as_view(), name='update'),
-    path('<int:pk>/deleete/', views.UserDeleteView.as_view(), name='delete'),
+    path('<int:pk>/delete/', views.UserDeleteView.as_view(), name='delete'),
     path('logout/', logout_view, name='logout'),
 ]
